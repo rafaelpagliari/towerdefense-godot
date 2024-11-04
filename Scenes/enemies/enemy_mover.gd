6a175f8cceb82e5ccbece292eb6957f8d0eaaa0e
Rafael Pagliari <rafaelpagliari15@hotmail.com>
2024-11-04 19:30:54 -0300

    Adicionado botao de velocidade e funcao nas waves e enemy para aplicar a velocidade

--- Scenes/enemies/enemy_mover.gd
+++ Scenes/enemies/enemy_mover.gd
@@ -23,7 +23,7 @@ func _ready():
 func _process(_delta):
 	if state == State.walking:
 		#Move
-		progress_ratio += 0.0005 * speed
+		progress_ratio += 0.0005 * speed * Engine.time_scale
 		if progress_ratio == 1:
 			finished_path()
 			return
--- Scenes/ui/hud.gd
+++ Scenes/ui/hud.gd
@@ -3,14 +3,36 @@ extends Control
 var next_wait_time := 0
 var waited := 0
 var open_details_pane : PanelContainer
+var speed_levels := [1, 3, 5]  # Define as velocidades 1x, 2x e 3x e 10x
+var current_speed_level := 0  # Começa com a velocidade normal (1x)
 
-func	 _ready():
+@onready var speed_button := $SpeedButton
+
+
+func _ready():
 	Globals.hud = self
 	Globals.baseHpChanged.connect(update_hp)
 	Globals.goldChanged.connect(update_gold)
 	Globals.waveStarted.connect(show_wave_count)
 	Globals.waveCleared.connect(show_wave_timer)
 	Globals.enemyDestroyed.connect(update_enemy_count)
+
+# Conecta o botão SpeedButton ao método para alternar a velocidade
+	speed_button.connect("pressed", Callable(self, "_on_speed_button_pressed"))
+
+
+func _on_speed_button_pressed():
+	# Alterna entre as velocidades 1x, 2x e 3x
+	current_speed_level = (current_speed_level + 1) % speed_levels.size()
+	var new_speed = speed_levels[current_speed_level]
+	# Atualiza o texto do botão para mostrar a velocidade atual
+	speed_button.text = str(new_speed) + "x"
+	# Ajusta a velocidade do jogo
+	set_game_speed(new_speed)
+
+func set_game_speed(multiplier):
+	# Ajusta o tempo do jogo
+	Engine.time_scale = multiplier
 
 func update_hp(newHp, maxHp):
 	%HPLabel.text = "HP: "+str(round(newHp))+"/"+str(round(maxHp))
